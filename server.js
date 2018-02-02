cbab53505f50272217d35b9475a4ce1d88d3baf9
Mauricio Melo <souza.msmauricio@gmail.com>
2018-02-02 13:15:46 -0200

    https attemp

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 var https = require('https');
-var sslRedirect = require('heroku-ssl-redirect');
 var app = require('./config/express');
 require('./config/database')('mongodb://opamobi:123@ds249787.mlab.com:49787/heroku_jnjgbv0t');
-
-// enable ssl redirect
-app.use(sslRedirect());
-
-app.listen(3000);
 
 var port = process.env.PORT || 5555;
 
