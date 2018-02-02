cbdbe0d9162b23af1e36ad6052994a7b2e7dd62b
Mauricio Melo <souza.msmauricio@gmail.com>
2018-02-02 13:21:25 -0200

    http fallback

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var https = require('https');
+var http = require('http');
 var app = require('./config/express');
 require('./config/database')('mongodb://opamobi:123@ds249787.mlab.com:49787/heroku_jnjgbv0t');
 
 var port = process.env.PORT || 5555;
 
-https.createServer(app)
+http.createServer(app)
 .listen(port, function() {
 	console.log('Servidor Opa! [ON]');
 });
