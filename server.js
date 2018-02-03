111155faaf0abb3a0d135d7af297d282edd7b504
Mauricio Melo <souza.msmauricio@gmail.com>
2018-02-03 15:40:42 -0200

    server config

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var https = require('https');
+var http = require('http');
 var app = require('./config/express');
+var fs = require('fs');
+
 require('./config/database')('mongodb://opamobi:123@ds249787.mlab.com:49787/heroku_jnjgbv0t');
 
-var port = process.env.PORT || 5555;
+var port = process.env.PORT || 80;
 
-https.createServer(app)
+http.createServer(app)
 .listen(port, function() {
 	console.log('Servidor Opa! [ON]');
 });
+
+https.createServer(app).listen(443);
