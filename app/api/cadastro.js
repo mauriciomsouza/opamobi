ec2acc412161b41b026125e9e251b7a49664de7b
Mauricio Melo <souza.msmauricio@gmail.com>
2018-02-07 15:58:07 -0200

    mail setup

--- app/api/cadastro.js
+++ app/api/cadastro.js
@@ -60,7 +60,7 @@ module.exports = function(app) {
                 }
                 console.log('Message sent: %s', info.messageId);
                 // Preview only available when sending through an Ethereal account
-                nodemailer.getTestMessageUrl(info));
+                nodemailer.getTestMessageUrl(info);
 
                 // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
                 // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
